452d460203ebd3b09652e03cba7f51d5e030c258
andres <valentinomonzonandres@gmail.com>
2023-09-02 21:49:15 -0300

    configuro el eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
-
-function App() {
-
+function App () {
   return (
-    <div>
-      hola mundo
-    </div>
+    <main className='w-full h-screen flex justify-center items-center'>
+      <h1>Obtener Colores</h1>
+      <div>
+        <h2>Ingrese una imagen</h2>
+        <input type="file" />
+      </div>
+    </main>
   )
 }
 
