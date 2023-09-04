11a9e4de66a0771e4f12ad924c837f5311fe073b
andres <valentinomonzonandres@gmail.com>
2023-09-04 16:49:21 -0300

    finalizo la funcionalidad de obtener la paleta de colores

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { IconX } from '@tabler/icons-react'
+import { IconMoodWrrr, IconX } from '@tabler/icons-react'
+import { GetColor } from './components/GetColor/GetColor'
 
 function App() {
   const [image, setImage] = useState<string | null>(null)
@@ -14,32 +15,47 @@ function App() {
         const result = e.target?.result as string
         setImage(result)
       }
+      e.target.value = ''
     }
   }
 
+  const clearImage = () => {
+    setImage(null)
+  }
+
   return (
-    <main className='bg-black w-full h-screen text-white flex flex-col gap-5 justify-center p-10 items-center'>
-      <div className='flex flex-col w-full h-full bg-zinc-700 gap-3 p-5 rounded-md'>
-        <div className='h-min grid grid-cols-3 text-center'>
-          <h1 className='text-4xl col-span-2'>Obtener Paleta de colores</h1>
-          <h2 className='text-4xl col-span-1'>Colores</h2>
+    <main className='w-full h-screen flex flex-col gap-5 justify-center p-10 items-center bg-[#FFFFFF]'>
+      <div className='flex flex-col w-full h-full gap-3 p-5 rounded-md bg-[#d9d3c3] drop-shadow-lg'>
+        <div className='flex items-center text-black'>
+          <h1 className='text-3xl font-bold'>ColorPixel</h1>
         </div>
-        <div className='grid grid-cols-3 gap-2 h-full'>
-          <div className='h-full border-dashed rounded-md border-2 col-span-2 relative'>
-            <input onChange={(e) => { handleFile(e) }} className='outline-none m-0 p-0 cursor-pointer opacity-0 h-full w-full relative z-[20]' type='file' accept='image/*' />
-            <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl z-[10]'>Seleccione o arrastre una imagen</p>
+        <div className='h-full flex gap-2'>
+          <div className={`h-full w-1/2 flex items-center justify-center rounded-md relative  ${image != null ? '' : 'border-2 border-black hover:bg-[#00000055] duration-200'}`}>
+            <input onChange={(e) => { handleFile(e) }} className='absolute outline-none m-0 p-0 cursor-pointer opacity-0 h-full w-full' type='file' accept='image/*' />
+            <p className='text-2xl text-center'>Seleccione o arrastre una imagen</p>
             {
               image != null &&
-              <div className='w-full h-full absolute top-0 rounded-md z-[30]'>
-                <button onClick={() => setImage(null)} className='absolute right-3 top-3 text-black'>
-                  <IconX size='35' className='bg-white rounded-full p-2' />
+              <div className='w-full h-full absolute top-0 rounded-md drop-shadow-xl'>
+                <button onClick={clearImage} className='absolute right-3 top-3 text-black'>
+                  <IconX size='35' className='bg-white rounded-full p-2 hover:bg-[#000000] hover:text-white duration-200' />
                 </button>
-                <img src={image} alt='image' className='w-full h-full object-cover' />
+                <img src={image} alt='image' className='w-full h-full rounded-md object-cover' />
               </div>
             }
           </div>
-          <div className='col-span-1 border-2 p-2 rounded-md'>
-            colores
+
+          <div className='w-1/2'>
+            {
+              image != null ?
+                <div className='h-2/3'>
+                  <GetColor imageUrl={image} />
+                </div>
+                :
+                <div className='h-2/3 flex items-center justify-center flex-col'>
+                  <IconMoodWrrr size={100} />
+                  <p className='text-2xl'>Esperando imagen</p>
+                </div>
+            }
           </div>
         </div>
       </div>
---
+++ src/components/GetColor/GetColor.tsx
@@ -0,0 +1,68 @@
+import { IconCheck } from '@tabler/icons-react';
+import { Palette } from 'color-thief-react'
+import { useState } from 'react'
+import './GetColor.css'
+
+const Loading = () => {
+  return <p className='text-2xl'>Loading...</p>
+}
+
+export const GetColor = ({ imageUrl }: { imageUrl: string }) => {
+  const [copy, setCopy] = useState('')
+
+  const colorText = (hexColor: string) => {
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+    return luminance > 0.5;
+  }
+
+  const copiText = (color: string) => {
+    navigator.clipboard.writeText(color)
+    setCopy(color)
+  }
+
+  return (
+    <Palette
+      src={imageUrl}
+      crossOrigin='anonymous'
+      format='hex'
+      colorCount={5}
+    >
+      {
+        ({ data, loading }) => {
+          if (loading) {
+            return <Loading />
+          }
+          return (
+            <div className='h-full' style={{ filter: 'drop-shadow(0px 25px 25px rgba(0, 0, 0, 0.25))' }}>
+              <ul className='flex h-full'>
+                {
+                  data?.map((color, index) => (
+                    <li key={index} className='flex gap-2 items-center flex-1'>
+                      <div style={{ backgroundColor: color, }} className='p-5 flex-1 flex justify-center h-full relative'>
+                        {
+                          copy === color ? <IconCheck
+                            onMouseLeave={() => setCopy('')}
+                            className={`animation-aparecer absolute bottom-2 h-[32px] p-1 w-full ${colorText(color) ? 'text-black' : 'text-white'}`} /> :
+                            <p
+                              onClick={() => copiText(color)}
+                              className={`absolute bottom-2 font-bold p-1 rounded-md duration-200 cursor-pointer ${colorText(color) ? 'text-black hover:bg-[#00000022]' : 'text-white hover:bg-[#ffffff22]'}`}>
+                              {color}
+                            </p>
+                        }
+                      </div>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+          )
+        }
+      }
+    </Palette>
+  )
+};
